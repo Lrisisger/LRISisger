7b20a413fd15480834eaae417874d688f2b4b4cf
Luís Couto <77895438+luisaraujoc@users.noreply.github.com>
2023-10-16 19:33:00 +0000

    fix reception

--- app/views/all/reception.php
+++ app/views/all/reception.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/css/all/reception.css">
+    <link rel="stylesheet" href="../../../public/css/general/main.css">
     <title>LRISisger</title>
 </head>
 
